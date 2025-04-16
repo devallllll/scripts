1f73e2c1b75489b8cdb658459bbc14b613f060c1
devallllll <93842433+devallllll@users.noreply.github.com>
2025-04-16 11:03:18 +0200

    Update DellCommandUpdate.ps1
    Spoofed browser as something blocks it for me otherwise

--- scripts/DellCommandUpdate.ps1
+++ scripts/DellCommandUpdate.ps1
@@ -1,89 +1,3 @@
-<#
-  .SYNOPSIS
-    Installs Dell updates via Dell Command Update
-  .DESCRIPTION
-    Installs the latest version of Dell Command Update and applies all Dell updates silently.
-  .LINK
-    https://www.dell.com/support/product-details/en-us/product/command-update/resources/manuals
-    https://github.com/wise-io/scripts/blob/main/scripts/DellCommandUpdate.ps1
-  .NOTES
-    Author: Aaron J. Stevenson
-#>
-
-function Get-Architecture {
-  # On PS x86, PROCESSOR_ARCHITECTURE reports x86 even on x64 systems.
-  # To get the correct architecture, we need to use PROCESSOR_ARCHITEW6432.
-  # PS x64 doesn't define this, so we fall back to PROCESSOR_ARCHITECTURE.
-  # Possible values: amd64, x64, x86, arm64, arm
-  if ($null -ne $ENV:PROCESSOR_ARCHITEW6432) { $Architecture = $ENV:PROCESSOR_ARCHITEW6432 }
-  else {     
-    if ((Get-CimInstance -ClassName CIM_OperatingSystem -ErrorAction Ignore).OSArchitecture -like 'ARM*') {
-      if ( [Environment]::Is64BitOperatingSystem ) { $Architecture = 'arm64' }  
-      else { $Architecture = 'arm' }
-    }
-
-    if ($null -eq $Architecture) { $Architecture = $ENV:PROCESSOR_ARCHITECTURE }
-  }
-
-  switch ($Architecture.ToLowerInvariant()) {
-    { ($_ -eq 'amd64') -or ($_ -eq 'x64') } { return 'x64' }
-    { $_ -eq 'x86' } { return 'x86' }
-    { $_ -eq 'arm' } { return 'arm' }
-    { $_ -eq 'arm64' } { return 'arm64' }
-    default { throw "Architecture '$Architecture' not supported." }
-  }
-}
-
-function Get-InstalledApps {
-  param(
-    [Parameter(Mandatory)][String[]]$DisplayNames,
-    [String[]]$Exclude
-  )
-  
-  $RegPaths = @(
-    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-    'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-  )
-  
-  # Get applications matching criteria
-  $BroadMatch = @()
-  foreach ($DisplayName in $DisplayNames) {
-    $AppsWithBundledVersion = Get-ChildItem -Path $RegPaths | Get-ItemProperty | Where-Object { $_.DisplayName -like "*$DisplayName*" -and $null -ne $_.BundleVersion }
-    if ($AppsWithBundledVersion) { $BroadMatch += $AppsWithBundledVersion }
-    else { $BroadMatch += Get-ChildItem -Path $RegPaths | Get-ItemProperty | Where-Object { $_.DisplayName -like "*$DisplayName*" } }
-  }
-  
-  # Remove excluded apps
-  $MatchedApps = @()
-  foreach ($App in $BroadMatch) {
-    if ($Exclude -notcontains $App.DisplayName) { $MatchedApps += $App }
-  }
-
-  return $MatchedApps | Sort-Object { [version]$_.BundleVersion } -Descending
-}
-
-function Remove-IncompatibleApps {
-  # Check for incompatible products
-  $IncompatibleApps = Get-InstalledApps -DisplayNames 'Dell Update', 'Dell Command | Update' `
-    -Exclude 'Dell SupportAssist OS Recovery Plugin for Dell Update', 'Dell Command | Update for Windows Universal'
-  foreach ($App in $IncompatibleApps) {
-    Write-Output "Attempting to remove program: [$($App.DisplayName)]"
-    try {
-      if ($App.UninstallString -match 'msiexec') {
-        $Guid = [regex]::Match($App.UninstallString, '\{[0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}\}').Value
-        Start-Process -NoNewWindow -Wait -FilePath 'msiexec.exe' -ArgumentList "/x $Guid /quiet /qn"
-      }
-      else { Start-Process -NoNewWindow -Wait -FilePath $App.UninstallString -ArgumentList '/quiet' }
-      Write-Output "Successfully removed $($App.DisplayName)"
-    }
-    catch { 
-      Write-Warning "Failed to remove $($App.DisplayName)"
-      Write-Warning $_
-      exit 1
-    }
-  }
-}
-
 function Install-DellCommandUpdate {
   function Get-LatestDellCommandUpdate {
     # Set KB URL
@@ -99,6 +13,7 @@ function Install-DellCommandUpdate {
       'accept'          = 'text/html'
       'accept-encoding' = 'gzip'
       'accept-language' = '*'
+      'User-Agent'      = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
     }
   
     # Attempt to parse Dell website for download page links of latest DCU
@@ -138,9 +53,13 @@ function Install-DellCommandUpdate {
     # Download installer
     Write-Output "`nDell Command Update installation needed"
     Write-Output 'Downloading...'
-    Invoke-WebRequest -Uri $LatestDellCommandUpdate.URL -OutFile $Installer -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
+    
+    # Create WebClient with spoofed user agent
+    $wc = New-Object System.Net.WebClient
+    $wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36")
+    $wc.DownloadFile($LatestDellCommandUpdate.URL, $Installer)
 
-    # Install .NET
+    # Install DCU
     Write-Output 'Installing...'
     Start-Process -Wait -NoNewWindow -FilePath $Installer -ArgumentList '/s'
 
@@ -158,95 +77,3 @@ function Install-DellCommandUpdate {
   }
   else { Write-Output "`nDell Command Update installation / upgrade not needed`n" }
 }
-
-function Install-DotNetDesktopRuntime {
-  function Get-LatestDotNetDesktopRuntime {
-    $BaseURL = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop'
-    $Version = (Invoke-WebRequest -Uri "$BaseURL/LTS/latest.version" -UseBasicParsing).Content
-    $Arch = Get-Architecture
-    $URL = "$BaseURL/$Version/windowsdesktop-runtime-$Version-win-$Arch.exe"
-  
-    return @{
-      URL     = $URL
-      Version = $Version
-    }
-  }
-  
-  $LatestDotNet = Get-LatestDotNetDesktopRuntime
-  $Installer = Join-Path -Path $env:TEMP -ChildPath (Split-Path $LatestDotNet.URL -Leaf)
-  $CurrentVersion = (Get-InstalledApps -DisplayName 'Microsoft Windows Desktop Runtime').BundleVersion
-  if ($CurrentVersion -is [system.array]) { $CurrentVersion = $CurrentVersion[0] }
-  Write-Output "`nInstalled .NET Desktop Runtime: $CurrentVersion"
-  Write-Output "Latest .NET Desktop Runtime: $($LatestDotNet.Version)"
-
-  if ($CurrentVersion -lt $LatestDotNet.Version) {
-    
-    # Download installer
-    Write-Output "`n.NET Desktop Runtime installation needed"
-    Write-Output 'Downloading...'
-    Invoke-WebRequest -Uri $LatestDotNet.URL -OutFile $Installer
-
-    # Install .NET
-    Write-Output 'Installing...'
-    Start-Process -Wait -NoNewWindow -FilePath $Installer -ArgumentList '/install /quiet /norestart'
-
-    # Confirm installation
-    $CurrentVersion = (Get-InstalledApps -DisplayName 'Microsoft Windows Desktop Runtime').BundleVersion
-    if ($CurrentVersion -is [system.array]) { $CurrentVersion = $CurrentVersion[0] }
-    if ($CurrentVersion -match $LatestDotNet.Version) {
-      Write-Output "Successfully installed .NET Desktop Runtime [$CurrentVersion]"
-      Remove-Item $Installer -Force -ErrorAction Ignore 
-    }
-    else {
-      Write-Warning ".NET Desktop Runtime [$($LatestDotNet.Version)] not detected after installation attempt"
-      Remove-Item $Installer -Force -ErrorAction Ignore 
-      exit 1
-    }
-  }
-  else { Write-Output "`n.NET Desktop Runtime installation / upgrade not needed" }
-}
-
-function Invoke-DellCommandUpdate {
-  # Check for DCU CLI
-  $DCU = (Resolve-Path "$env:SystemDrive\Program Files*\Dell\CommandUpdate\dcu-cli.exe").Path
-  if ($null -eq $DCU) {
-    Write-Warning 'Dell Command Update CLI was not detected.'
-    exit 1
-  }
-  
-  try {
-    # Configure DCU automatic updates
-    Start-Process -NoNewWindow -Wait -FilePath $DCU -ArgumentList '/configure -scheduleAction=DownloadInstallAndNotify -updatesNotification=disable -forceRestart=disable -scheduleAuto -silent'
-    
-    # Scan for / apply updates
-    Start-Process -NoNewWindow -Wait -FilePath $DCU -ArgumentList '/scan -silent'
-    Start-Process -NoNewWindow -Wait -FilePath $DCU -ArgumentList '/applyUpdates -autoSuspendBitLocker=enable -reboot=disable'
-  }
-  catch {
-    Write-Warning 'Unable to apply updates using the dcu-cli.'
-    Write-Warning $_
-    exit 1
-  }
-}
-
-# Set PowerShell preferences
-Set-Location -Path $env:SystemRoot
-$ProgressPreference = 'SilentlyContinue'
-$ErrorActionPreference = 'Stop'
-if ([Net.ServicePointManager]::SecurityProtocol -notcontains 'Tls12' -and [Net.ServicePointManager]::SecurityProtocol -notcontains 'Tls13') {
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-}
-
-# Check device manufacturer
-if ((Get-CimInstance -ClassName Win32_BIOS).Manufacturer -notlike '*Dell*') {
-  Write-Output "`nNot a Dell system. Aborting..."
-  exit 0
-}
-
-# Handle Prerequisites / Dependencies
-Remove-IncompatibleApps
-Install-DotNetDesktopRuntime
-
-# Install DCU and available updates
-Install-DellCommandUpdate
-Invoke-DellCommandUpdate
